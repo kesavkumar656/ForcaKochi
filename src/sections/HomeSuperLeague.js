a72718199ea112094a3a2daf717385b0b709c624
Mahalakshmi Karmegam <mahalakshmi.karmegam@ting.in>
2024-09-13 16:07:19 +0530

    Super League Section

---
+++ src/sections/HomeSuperLeague.js
@@ -0,0 +1,22 @@
+// MODULES //
+
+// COMPONENTS //
+
+// SECTIONS //
+
+// PLUGINS //
+
+// UTILS //
+
+// STYLES //
+import styles from "@/styles/sections/HomeSuperLeague.module.scss";
+
+// IMAGES //
+
+// DATA //
+
+/** HomeSuperLeague Section */
+export default function HomeSuperLeague() {
+	return <section className={styles.HomeSuperLeague}></section>;
+}
+        
