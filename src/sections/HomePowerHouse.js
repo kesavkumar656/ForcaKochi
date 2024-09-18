98606964cf1b77a6241c4deb6d4664fa2a768062
kesavkumar656 <kesava.kumar@ting.in>
2024-09-18 16:42:44 +0530

    update

--- src/sections/HomePowerHouse.js
+++ src/sections/HomePowerHouse.js
@@ -101,7 +101,7 @@ export default function HomePowerHouse() {
 				<div className={`${styles.playerTable} section_spacing`}>
 					<Swiper
 						spaceBetween={50}
-						slidesPerView={4}
+						slidesPerView={5}
 						navigation={false}
 						pagination={{ clickable: true }}
 						// modules={(Navigation, Pagination, Scrollbar)}
